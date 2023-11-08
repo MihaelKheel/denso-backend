f79563c186eaf975d3efc5881f1caa8c0ceba460
Ace Gabriel P. Pasiliao <99068659+sarapmoace@users.noreply.github.com>
2023-11-08 16:36:41 +0800

    hard code: db string

--- src/ConnectDB.js
+++ src/ConnectDB.js
@@ -2,9 +2,12 @@ const DBConfig = require('./services/shared/config-service').getConf('DB')
 
 const mongoose = require('mongoose');
 
+const DB = process.env.MONGO_URI.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(DBConfig.DatabaseUrl);
+        // const conn = await mongoose.connect(DBConfig.DatabaseUrl);
+        const conn = await mongoose.connect("mongodb+srv://geoacepasiliao:pogingace@cluster0.qkr6w09.mongodb.net/denso?retryWrites=true&w=majority");
 
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     }catch (error){
